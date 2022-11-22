44b87ca9e7b067de875274b909ee65333c360917
SamNiehaus <97834000+SamNiehaus@users.noreply.github.com>
2022-11-22 15:37:22 +0100

    Index
    Index include nu een challenge, 3 aangemaakt voor testing

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 
-for ($i = 0; $i < 100; $i++){
-    echo 'Hello World ';
-}
+//for ($i = 0; $i < 100; $i++){
+//    echo 'Hello World ';
+//}
+
+?>
+
+<html>
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+
+<?php
+
+    $chosenChallenge = rand(1, 3);
+
+    include "challenges/Challenge1.php";
+
+?>
+
+</body>
+</html>
