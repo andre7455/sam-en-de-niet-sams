6d5b211e4bcde2f2404ed0fd52b74019b1a9ed0c
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-11-22 23:00:02 +0100

    fixed the code sam wrote, added comments and made it a set it and forget it setup

--- index.php
+++ index.php
@@ -1,25 +1,36 @@
 <?php
 
-//for ($i = 0; $i < 100; $i++){
-//    echo 'Hello World ';
-//}
-
 ?>
-
 <html>
+<!-- this is HTML code, this is used to tell the web browser what to do. this is called "static code".
+what i mean by that is that this code is not interactive with the user.
+for more information read learning/sources w3schools and search for HTML -->
 <head>
+	<title>Sam's simple challenge </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
 
+<!-- this is where the php code starts.
+Here we first difine a random number
+$ is used to declare a variable and because php is a soft language we do not have to define what type of data is put in the variable
+
+The include is used to import a other page.
+-->
 <?php
 
-    $chosenChallenge = rand(1, 3);
+	//initializing a filepath into a variable and generating a random int to pick a random challenge, after that getting the page.
+	//
+	//rand(1, count(glob("challenges/" . "*")))
+	//the rand operator generates a random number where 1 is the lowest and count(glob("challenges/" . "*")) returns the amount of files
+	//in the challenges directory, that way no matter how many files are in the directory there is one chosen out of all of them
 
-    include "challenges/Challenge1.php";
-
+	$chosenChallenge = "challenges" . "/Challenge" . rand(1, count(glob("challenges/" . "*"))) .".php";
+    include $chosenChallenge;
 ?>
 
+
+<!-- here we close out the html so that the browser knows not to expect more-->
 </body>
 </html>
