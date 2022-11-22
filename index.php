ddb9f047d103f1e1c0e0f54217014db734de874e
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-11-22 23:23:30 +0100

    test edit for myself

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 what i mean by that is that this code is not interactive with the user.
 for more information read learning/sources w3schools and search for HTML  -->
 <head>
-	<title>Sam's simple challenge </title>
+	<title>Sam's board</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
