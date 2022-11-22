d067089607c441681328a39051ad0b27d1414472
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-11-22 23:00:55 +0100

    fixed push issue

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <html>
 <!-- this is HTML code, this is used to tell the web browser what to do. this is called "static code".
 what i mean by that is that this code is not interactive with the user.
-for more information read learning/sources w3schools and search for HTML -->
+for more information read learning/sources w3schools and search for HTML  -->
 <head>
 	<title>Sam's simple challenge </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
