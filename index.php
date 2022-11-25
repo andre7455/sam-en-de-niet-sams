b207065de103edb7f63ed169100a223b7f6258d0
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-11-25 14:16:55 +0100

    small formatting changes

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ for more information read learning/sources w3schools and search for HTML  -->
 </head>
 
 <body>
-
 <!-- this is where the php code starts.
 Here we first difine a random number
 $ is used to declare a variable and because php is a soft language we do not have to define what type of data is put in the variable
@@ -19,13 +18,11 @@ $ is used to declare a variable and because php is a soft language we do not hav
 The include is used to import a other page.
 -->
 <?php
-
 	//initializing a filepath into a variable and generating a random int to pick a random challenge, after that getting the page.
 	//
 	//rand(1, count(glob("challenges/" . "*")))
 	//the rand operator generates a random number where 1 is the lowest and count(glob("challenges/" . "*")) returns the amount of files
 	//in the challenges directory, that way no matter how many files are in the directory there is one chosen out of all of them
-
 	$chosenChallenge = "challenges" . "/Challenge" . rand(1, count(glob("challenges/" . "*"))) .".php";
     include $chosenChallenge;
 ?>
