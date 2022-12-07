6d249fa3319a4c7dc42f32cd94eb56467a8f5897
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-12-07 13:56:20 +0100

    challenge 5 is almost done

--- challenges/Challenge5.php
+++ challenges/Challenge5.php
@@ -4,36 +4,12 @@
 $min = 1;
 $max = 10;
 $secretNumber = rand($min, $max);
-$numGuesses = 0;
-$maxNumGuesses = 5;
-$visible = true;
+$remainingGuesses = 5;
 
-function checkGuess($guess) {
-  global $secretNumber;
-  return $guess == $secretNumber;
-}
-
-echo "Welcome to the PHP skill game!\n";
-echo "I'm thinking of a number between $min and $max. Can you guess what it is?\n";
+echo "Welcome to the PHP skill game! </br>";
+echo "I'm thinking of a number between $min and $max. Can you guess what it is? </br>";
+echo '<a class="btn btn-primary" onclick="challenge(1,2)"> start mij!</a></br>';
+echo '<p id="remainingGuesses">you have ' . $remainingGuesses . ' remaining';
 ?>
-<a onclick="test()"> Klik mij!</a>
-
-<?php
-function checkButton(){
-  if($visible){
-    echo "Enter your guess: ";
-    if (checkGuess($guess)) {
-      echo "Congratulations, you guessed the correct number!/n";
-    } else {
-      echo "Sorry, that's not the correct number.\n";
-      $numGuesses++;
-    }
-  }
-}
-
-
-if ($numGuesses >= $maxNumGuesses) {
-  echo "Sorry, you ran out of guesses. The correct number was $secretNumber.\n";
-  echo "Game over.\n";
-}
-
+<label>antwoord</label>
+<input id="textField"></input>
--- js/Challenge5_helper.js
+++ js/Challenge5_helper.js
@@ -1,3 +1,13 @@
-function test(kaas){
-    alert(kaas);
+var guessesTaken;
+var secretNumberInStorage;
+
+function challenge(secretNumber, guessesIntotal){
+    if(1 = 1){
+        for(i = 0; i < 10; i--){
+            alert("you shall not guess agian!");
+        }
+    } else{
+        if(secretNumber == document.getElementById("textField").value);
+    }
+    guessesTaken++;
 }
