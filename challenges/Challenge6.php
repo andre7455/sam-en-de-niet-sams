18c31affd0e4bf69397de6af72c4b2ec45d18e05
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-12-12 13:47:19 +0100

    making examples

--- challenges/Challenge6.php
+++
@@ -1,9 +0,0 @@
-<div>
-    <h1>AAAHAHAHAHAHHA</h1>
-
-
-    <a>Wie is de precident van amerika?</a>
-    <button onclick="alert('fout')">henk</button>
-    <button onclick="alert('fout')">jan</button>
-    <button onclick="alert('goed')">piet</button>
-</div>
