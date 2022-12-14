78c949e6fcfb3213c5615896cbd110feb42bec28
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-12-14 10:22:36 +0100

    Update voorbeeldJaNeeComplex.php

--- voorbeelden/voorbeeldJaNeeComplex.php
+++ voorbeelden/voorbeeldJaNeeComplex.php
@@ -3,12 +3,16 @@ include "includes/header.php";
 ?>
 
 <p>Do you like chocolate?</p>
-  <form action="voorbeelden/voorbeeldJaNeeComplexController.php" method="post">
+  <form action="voorbeeldJaNeeComplexController.php" method="post">
     <input type="submit" name="answer" value="Yes" />
     <input type="submit" name="answer" value="No" />
   </form>
 </p>
 
 <?php
+//The form has two submit buttons, one for "Yes" and one for "No". 
+//When the user clicks one of the buttons, the form is submitted to the voorbeeldJaNeeComplexController.php file using the POST method. 
+//The values of the buttons are sent to the server in the answer parameter, so the server can determine which button the user clicked.
+
 include "includes/footer.php";
 ?>
