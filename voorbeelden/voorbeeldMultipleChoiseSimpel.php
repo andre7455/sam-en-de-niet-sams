ce585404b89bdd8f5634207c24ddc39224d7a0b4
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-12-14 10:30:52 +0100

    Update voorbeeldMultipleChoiseSimpel.php

--- voorbeelden/voorbeeldMultipleChoiseSimpel.php
+++ voorbeelden/voorbeeldMultipleChoiseSimpel.php
@@ -1,9 +1,11 @@
 <?php
     include "includes/header.php";
 ?>
+
+
 <div>
-    <h1>AAAHAHAHAHAHHA</h1>
-    <a>Wie is de precident van amerika?</a>
+    <h1>Dit is een titel</h1>
+    <a>dit is normale text, Wie is de precident van amerika?</a>
     <button onclick="alert('fout')">henk</button>
     <button onclick="alert('fout')">jan</button>
     <button onclick="alert('goed')">piet</button>
