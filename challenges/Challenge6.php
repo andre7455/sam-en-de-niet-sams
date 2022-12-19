df943e15099f8233be4dd30a6964240994003264
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-12-19 13:58:00 +0100

    new challenge

---
+++ challenges/Challenge6.php
@@ -0,0 +1,21 @@
+<?php
+ include "includes/header.php";
+?>
+<body>
+  <div id="challenge" style="color: white; background-color: green;">klik!</div>
+  <button id="button" onclick="onButtonClick()">click me</button>
+  <script>
+    let failTimeout;
+    function onButtonClick() {
+      clearTimeout(failTimeout);
+      document.getElementById("challenge").innerHTML = "sample text";
+    }
+    failTimeout = setTimeout(function() {
+      document.getElementById("challenge").style.backgroundColor = "red";
+      document.getElementById("challenge").innerHTML = "fail";
+    }, 500);
+  </script>
+</body>
+<?php
+ include "includes/footer.php";
+?>
