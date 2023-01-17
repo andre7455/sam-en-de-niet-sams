f8ab6658d1e0e9c98db18947b75354992d2704e1
SamNiehaus <97834000+SamNiehaus@users.noreply.github.com>
2023-01-17 15:18:08 +0100

    challenges
    5 nieuwe

---
+++ challenges/Challenge15.php
@@ -0,0 +1,6 @@
+<p>Wat is de hoogste berg in Afrika?</p>
+
+<button onclick="generatePunishment()" type="button" name="button1" id="button1" class="btn btn-primary">Mt Everest</button>
+<button onclick="generateReward()" type="button" name="button2" id="button2" class="btn btn-primary">Kilimanjaro</button>
+<button onclick="generatePunishment()" type="button" name="button2" id="button2" class="btn btn-primary">Mt Fuji</button>
+<button onclick="generatePunishment()" type="button" name="button2" id="button2" class="btn btn-primary">Mawenzi</button>
