17a79f29b8241cb8c53744833f5e0af25681c8f0
SamNiehaus <97834000+SamNiehaus@users.noreply.github.com>
2023-01-17 15:05:52 +0100

    challenges
    1 nieuwe

---
+++ challenges/Challenge10.php
@@ -0,0 +1,8 @@
+<p>Druk op de groene knop</p>
+
+<button onclick="generatePunishment()" type="button" name="button1" id="button1" class="btn btn-primary">Rood</button>
+<button onclick="generateReward()" type="button" name="button2" id="button2" class="btn btn-success">Blauw</button>
+<button onclick="generatePunishment()" type="button" name="button2" id="button2" class="btn btn-warning">Groen</button>
+<button onclick="generatePunishment()" type="button" name="button2" id="button2" class="btn btn-danger">Geel</button>
+
+
