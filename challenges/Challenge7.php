33843d960212f9f1ec51f6e2e0fd89a57ef543ec
SamNiehaus <97834000+SamNiehaus@users.noreply.github.com>
2023-01-17 14:36:34 +0100

    Challenges
    2 edits

--- challenges/Challenge7.php
+++ challenges/Challenge7.php
@@ -4,8 +4,8 @@ include "includes/header.php";
 
     <p>Wanneer is de eerste elektrische computer uitgevonden?</p>
 
-    <button onclick="stapVooruit()" type="button" name="button1" id="button1" class="btn">1943</button>
-    <button onclick="generatePunishment()" type="button" name="button2" id="button2" class="btn">1978</button>
+    <button onclick="stapVooruit()" type="button" name="button1" id="button1" class="btn btn-primary">1943</button>
+    <button onclick="generatePunishment()" type="button" name="button2" id="button2" class="btn btn-primary">1978</button>
 
     <p id="text"></p>
 
--- challenges/Challenge8.php
+++ challenges/Challenge8.php
@@ -4,8 +4,8 @@ include "includes/header.php";
 
     <p>Wanneer is google opgericht? </p>
 
-    <button onclick="generatePunishment()" type="button" name="button1" id="button1" class="btn">1989</button>
-    <button onclick="stapVooruit()" type="button" name="button2" id="button2" class="btn">1998</button>
+    <button onclick="generatePunishment()" type="button" name="button1" id="button1" class="btn btn-primary">1989</button>
+    <button onclick="stapVooruit()" type="button" name="button2" id="button2" class="btn btn-primary">1998</button>
 
     <p id="text"></p>
 
