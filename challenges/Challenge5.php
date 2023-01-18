eac5cf0ec1fde42ea1a5c0a33946d8be42798316
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2023-01-18 09:38:26 +0100

    Update Challenge5.php

--- challenges/Challenge5.php
+++ challenges/Challenge5.php
@@ -5,13 +5,30 @@ $max = 10;
 $secretNumber = rand($min, $max);
 $remainingGuesses = 5;
 
-echo "Welcome to the PHP skill game! </br>";
-echo "I'm thinking of a number between $min and $max. Can you guess what it is? </br>";
-echo '<p id="remainingGuesses">you have ' . $remainingGuesses . ' remaining';
+echo "<p>I'm thinking of a number between $min and $max. Can you guess what it is?</p></br>";
+echo '<p id="remainingGuesses">you have ' . $remainingGuesses . ' remaining guesses</p>';
 ?>
-<script src="js/Challenge5_helper.js"></script>
 <button onclick="test()">Click me</button>
 <input id="textField"></input>
+<script>
+var secretNumber = <?php echo $secretNumber; ?>;
+var remainingGuesses = <?php echo $remainingGuesses; ?>;
 
+function test() {
+  var userGuess = document.getElementById("textField").value;
+  if (userGuess == secretNumber) {
+    alert("Congratulations, you guessed the correct number!");
+  } else if (userGuess > secretNumber) {
+    alert("Too high! Guess again.");
+    remainingGuesses--;
+  } else if (userGuess < secretNumber) {
+    alert("Too low! Guess again.");
+    remainingGuesses--;
+  }
+  document.getElementById("remainingGuesses").innerHTML = "you have " + remainingGuesses + " remaining guesses";
+  if (remainingGuesses == 0) {
+    alert("You have run out of guesses. The correct number was " + secretNumber + ".");
+  }
+}
+</script>
 <?php
-//TODO: fix deze andre
