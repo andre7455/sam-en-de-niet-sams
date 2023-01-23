3d42f773ae35c42d6aff5bf912451a3ea6e32545
SamNiehaus <97834000+SamNiehaus@users.noreply.github.com>
2023-01-23 19:22:37 +0100

    Challenges

---
+++ challenges/Challenge33.php
@@ -0,0 +1,13 @@
+<p>Who is that Pokemon?</p>
+
+
+<button onclick="generatePunishment()" type="button" name="button1" id="button1" class="btn btn-primary">Pikachu</button>
+<button onclick="generatePunishment()" type="button" name="button2" id="button2" class="btn btn-primary">Jigglypuff</button>
+<button onclick="generateReward()" type="button" name="button2" id="button2" class="btn btn-primary">Snorlax</button>
+<button onclick="generatePunishment()" type="button" name="button2" id="button2" class="btn btn-primary">Raichu</button>
+
+<br>
+<br>
+
+<img src="../Media/snorlax.jpg">
+
